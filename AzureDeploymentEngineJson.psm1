47866399474c190ea6f2297f9b870fe91353e6ab
Trond Hindenes <trond@hindenes.com>
2014-05-25 12:49:01 +0200

    updated module + readme

--- AzureDeploymentEngineJson.psm1
+++ AzureDeploymentEngineJson.psm1
@@ -5,8 +5,7 @@ add-type -Path "$psscriptroot\Dlls\AzureDeploymentEngine.dll"
 gci $psscriptroot\Nikolic-AzureHelpers\*.ps1 | % { . $_.FullName }
 gci $psscriptroot\Credentials\*.ps1 | % { . $_.FullName }
 gci $psscriptroot\PostDeploymentScripts\*.ps1 | % { . $_.FullName }
-gci $psscriptroot\ObjectFunctions2.ps1 | % { . $_.FullName }
-gci $psscriptroot\JsonFunctions.ps1 | % { . $_.FullName }
+gci $psscriptroot\ObjectFunctions\*.ps1 | % { . $_.FullName }
 gci $psscriptroot\Invoke-AzdeEnvironment.ps1 | % { . $_.FullName }
 gci $psscriptroot\Get-AzdeIntResultingSetting.ps1 | % { . $_.FullName }
 gci $psscriptroot\Enable-AzdeAzureSubscription.ps1 | % { . $_.FullName }
@@ -14,7 +13,6 @@ gci $psscriptroot\iaas\*.ps1 | % { . $_.FullName }
 gci $psscriptroot\helperfunctions\*.ps1 | % { . $_.FullName }
 gci $psscriptroot\AssertFunctions\*.ps1 | % { . $_.FullName }
 gci $psscriptroot\AzureBlobFunctions\*.ps1 | % { . $_.FullName }
-#Write-Output "This is the Azure Deployment Engine. Some code kindly borrowed from Aleksandar Nikolic"
 
 #Read ModuleSettings
 $modulesettingsJson = get-content "$psscriptroot\ModuleSettings.json" -raw
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Azure Deployment Engine (or AZdE) is a PowerShell module for automating the deployment of complex, multi-VM systems in Microsoft Azure.
 
-### What id does ###
+### What it does ###
 
 * Construct a deployment using PowerShell functions or by editing json
 * Import/Export to Json
@@ -10,4 +10,9 @@ Azure Deployment Engine (or AZdE) is a PowerShell module for automating the depl
 * Auto-deployment of domain controller VM
 * Run post-deployment scripts on one or several VMs to perform app installs
 * Copy files between local computer and Azure VMs using blob storage
+
+### What it doesn't do (yet) ###
+
+* Verify your inputs. Which may lead to errors while running the script, for instance if you specified an invalid cloudservice name
+* Use paralellism. The script will deploy VMs, one at a time, and then run scripts also one at a time. 
 
